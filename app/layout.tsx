3fc8bf1710b6d70dcee90196814f7a53c4ee4490
RETSKEY <bashlovka96@mail.ru>
2023-11-25 17:43:57 +0300

    added fonts variants 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={rubik.className}>
       <body>
         <Navbar />
         <main>{children}</main>
