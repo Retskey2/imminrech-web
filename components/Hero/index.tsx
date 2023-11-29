48e4a90a99bdeaab6e544e0a541285e3b8dc4a9e
RETSKEY <bashlovka96@mail.ru>
2023-11-29 17:31:41 +0300

    added paralax on starts

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -11,7 +11,12 @@ import { motion } from 'framer-motion';
 
 const Hero = () => {
 	const [position, setPosition] = useState({
-		star: { x: 0, y: 0 },
+		stars: {
+			star1: { x: 0, y: 0 },
+			star2: { x: 0, y: 0 },
+			star3: { x: 0, y: 0 },
+			star4: { x: 0, y: 0 }
+		},
 		iphone: { x: 0, y: 0 }
 	});
 
@@ -19,9 +24,23 @@ const Hero = () => {
 		const xPos = (e.clientX / window.innerWidth - 0.5) * 40;
 		const yPos = (e.clientY / window.innerHeight - 0.5) * 40;
 		setPosition({
-			star: {
-				x: xPos,
-				y: yPos
+			stars: {
+				star1: {
+					x: xPos * 0.7,
+					y: yPos * 0.5
+				},
+				star2: {
+					x: xPos * 0.5,
+					y: yPos * 1.2
+				},
+				star3: {
+					x: xPos * 0.2,
+					y: yPos * 1.3
+				},
+				star4: {
+					x: xPos * 1.3,
+					y: yPos * 2.1
+				}
 			},
 			iphone: {
 				x: xPos * 0.3,
@@ -67,6 +86,7 @@ const Hero = () => {
 					alt='back'
 					src={backgroundHeroPromo}
 				/>
+
 				<Image
 					className={styles.heroImageFront}
 					style={{
@@ -80,29 +100,26 @@ const Hero = () => {
 					height={500}
 					src={iphone.src}
 				/>
-				<div
-					className={styles.heroImageEffectsBlock}
-					style={{
-						transform: `translate(${position.star.x}px, ${position.star.y}px)`
-					}}
-					aria-hidden='true'
-				>
+				<div className={styles.heroImageEffectsBlock} aria-hidden='true'>
+					<motion.div>
+						<Image
+							style={{
+								transform: `translate(${position.stars.star1.x}px, ${position.stars.star1.y}px)`
+							}}
+							className='absolute left-24 top-32'
+							priority
+							draggable={false}
+							alt='iphone'
+							quality={100}
+							width={33}
+							height={33}
+							src={star.src}
+						/>
+					</motion.div>
+
 					<Image
 						style={{
-							transform: `translate(${position.star.x}px, ${position.star.y}px)`
-						}}
-						className='absolute left-24 top-32'
-						priority
-						draggable={false}
-						alt='iphone'
-						quality={100}
-						width={33}
-						height={33}
-						src={star.src}
-					/>
-					<Image
-						style={{
-							transform: `translate(${position.star.x}px, ${position.star.y}px)`
+							transform: `translate(${position.stars.star2.x}px, ${position.stars.star2.y}px)`
 						}}
 						className='absolute -right-6 top-10'
 						priority
@@ -115,7 +132,7 @@ const Hero = () => {
 					/>
 					<Image
 						style={{
-							transform: `translate(${position.star.x}px, ${position.star.y}px)`
+							transform: `translate(${position.stars.star3.x}px, ${position.stars.star3.y}px)`
 						}}
 						className='absolute right-6 top-[106px]'
 						priority
@@ -128,7 +145,7 @@ const Hero = () => {
 					/>
 					<Image
 						style={{
-							transform: `translate(${position.star.x}px, ${position.star.y}px)`
+							transform: `translate(${position.stars.star4.x}px, ${position.stars.star4.y}px)`
 						}}
 						className='absolute -left-10 top-[320px]'
 						priority
