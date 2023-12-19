adf4a0be7516f6c571f66cddf29dddd54f427b67
RETSKEY <bashlovka96@mail.ru>
2023-12-19 15:21:09 +0300

    changed metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Rubik } from 'next/font/google';
+import Script from 'next/script';
 
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
 import './globals.css';
-import { YandexMetrica } from '@/providers/YandexMetrica';
+import YandexMetrika, { YandexMetrica } from '@/providers/YandexMetrica';
 
 export const metadata: Metadata = {
 	title: 'Immintech',
@@ -24,13 +25,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang='en' className={rubik.className}>
 			<body>
+				<Script id='metrika-counter' strategy='afterInteractive'>
+					{`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(95920833, "init", {
+                    defer: true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`}
+				</Script>
 				<Navbar />
 				<main>
-					<YandexMetrica>
-						{children}
-						<SpeedInsights />
-						<Analytics />
-					</YandexMetrica>
+					<YandexMetrika />
+					{children}
+					<SpeedInsights />
+					<Analytics />
 				</main>
 				<Footer />
 			</body>
--- providers/YandexMetrica.tsx
+++ providers/YandexMetrica.tsx
@@ -1,36 +1,17 @@
 'use client';
 
-import { Router } from 'next/router';
-import { PropsWithChildren, useCallback, useEffect } from 'react';
-import ym, { YMInitializer } from 'react-yandex-metrika';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import ym from 'react-yandex-metrika';
 
-//Check, is it production mode
-export const enableYM = process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_YM_ID;
-
-export const YandexMetrica = ({ children }: PropsWithChildren) => {
-	const hit = useCallback((url: string) => {
-		if (enableYM) {
-			ym('hit', url);
-		} else {
-			console.log(`%c[YandexMetrika](HIT)`, `color: orange`, url);
-		}
-	}, []);
+export default function YandexMetrika() {
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
 
 	useEffect(() => {
-		hit(window.location.pathname + window.location.search);
-		Router.events.on('routeChangeComplete', (url: string) => hit(url));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	return (
-		<>
-			{enableYM && (
-				<YMInitializer
-					accounts={[Number(process.env.NEXT_PUBLIC_YM_ID)]}
-					options={{ webvisor: true, defer: true }}
-					version='2'
-				/>
-			)}
-			{children}
-		</>
-	);
-};
+		const url = `${pathname}?${searchParams}`;
+		ym(process.env.NEXT_PUBLIC_YM_ID || '', 'hit', url);
+	}, [pathname, searchParams]);
+
+	return null;
+}
